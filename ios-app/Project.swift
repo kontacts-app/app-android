a6a534f0c60c2dfa464f11acaac5733b0d6ca242
Răzvan Roșu <razvan.rosu@aton.com>
2024-04-11 18:21:17 +0200

    Remove iOS app sources

--- ios-app/Project.swift
+++
@@ -1,9 +0,0 @@
-import ProjectDescription
-import ProjectDescriptionHelpers
-
-// MARK: - Project
-
-// Creates our project using a helper function defined in ProjectDescriptionHelpers
-let project = Project.app(name: "Contacts",
-                          platform: .iOS,
-                          additionalTargets: ["Repository"])
--- ios-app/Targets/Repository/Tests/IosAppKitTests.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import XCTest
-
-final class IosAppKitTests: XCTestCase {
-    func test_example() {
-        XCTAssertEqual("IosAppKit", "IosAppKit")
-    }
-}
